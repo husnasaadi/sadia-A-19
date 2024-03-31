9a0fe17332496adab9fa79a75d5a09d96caed84b
sadia saadi <saadiasaadi944@gmail.com>
2024-03-31 08:27:05 +0500

    assignment-19

---
+++ app.js
@@ -0,0 +1,71 @@
+// assignment 19
+
+// chapter no 26(rounding numbers)
+
+// q1
+
+var roundNum = Math.round(71);
+
+// Q2
+
+var origNum = 5.6;
+var roundNum = Math.round(origNum);
+console.log(roundNum);
+
+// Q3
+
+var origNum = 10.8;
+var roundNum = Math.floor(origNum);
+console.log(roundNum);
+
+// Q4
+
+var origNum = 6.9
+var num = Math.round(origNum);
+console.log(num);
+
+// Q5
+
+var Num = Math.round(0.5);
+console.log(Num);
+
+// chapter no 27 ( Random numbers)
+
+// Q1
+
+var randomNum = Math.round(23);
+var numRnage = Math.floor(randomNum*50)+1;
+console.log(numRnage);
+
+// Q2
+
+var randomNum = 23;
+var numRnage = Math.round(randomNum);
+console.log(numRnage);
+
+// Q3
+
+var randomNum = 1;
+if (randomNum <0.5){
+    console.log("head");
+}
+else {
+    console.log("tail");
+}
+
+// Q4
+
+// chapter 28 & 29 (converting strings)
+
+// Q1
+
+var str = "45";
+var num = parseInt(str);
+console.log(num);
+
+// Q2
+
+var str = "123";
+var integer = parseInt(str);
+console.log(integer);
+
